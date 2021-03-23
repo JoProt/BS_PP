eeb48ccf846a0a96ec92da49ae358f0d911d6de0
Lukas Basedow <shou@shou.moe>
2021-03-23 19:13:26 +0100

    + find fingers

--- ppscan.py
+++ ppscan.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+import numpy as np
 import cv2 as cv
 
 
@@ -19,10 +20,49 @@ import cv2 as cv
 # Preprocessing #
 # # # # # # # # #
 
+def make_binary(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    lower = np.array([0, 48, 80], dtype = "uint8")
+    upper = np.array([20, 255, 255], dtype = "uint8")
+    skinRegionHSV = cv.inRange(hsv, lower, upper)
+    blurred = cv.GaussianBlur(skinRegionHSV,(5,5),0)
+    ret, thresh = cv.threshold(blurred,0,255,cv.THRESH_BINARY)
+    return thresh
 
-def find_fingers():
-    return
+def get_contours(mask_img):
+    contours, hierarchy = cv.findContours(mask_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours = max(contours, key=lambda x: cv.contourArea(x))
+    hull = cv.convexHull(contours)
+    return contours, hull
 
+def get_defects(contours):
+    hull = cv.convexHull(contours, returnPoints=False)
+    defects = cv.convexityDefects(contours, hull)
+    return defects
+
+def find_fingers(img):
+    mask_img = make_binary(img)
+    contours, hull = get_contours(mask_img)
+    finger_points = []
+    defects = get_defects(contours)
+    if defects is not None:
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i][0]
+            start = tuple(contours[s][0])
+            end = tuple(contours[e][0])
+            far = tuple(contours[f][0])
+            a = np.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = np.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = np.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            angle = np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
+
+            if angle <= np.pi / 2:
+                finger_points.append(far)
+
+    finger_points .pop(0)
+    finger_points .pop(1)
+
+    return finger_points #return list of two tuple
 
 def fit():
     return
