7807ffdd91ab360952ea45a19aa6c8944788af0f
Lorenz Gillner <l.gillner@stud.hs-wismar.de>
2021-03-23 18:38:06 +0100

    initial project structure

---
+++ ppscan.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Palmprint Scanner
+    =================
+    [your ad here]
+    
+    :license: who knows
+    :format: black, reST docstrings
+"""
+
+import os
+import sys
+import cv2 as cv
+
+
+# # # # # # # # #
+# Preprocessing #
+# # # # # # # # #
+
+
+def find_fingers():
+    return
+
+
+def fit():
+    return
+
+
+# ...
+
+
+def main():
+    print("it werks")
+    return
+
+
+if __name__ == "__main__":
+    sys.exit(main())
