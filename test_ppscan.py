780508375ad1d13133075b414b13ed75e98ef1fc
Christoph Werner <christoph@werners.name>
2021-04-14 19:54:25 +0200

    blacked all python files

--- test_ppscan.py
+++ test_ppscan.py
@@ -4,7 +4,6 @@ from parameterized import parameterized
 
 
 class TestPPscan(unittest.TestCase):
-
     def test_interpol2d_gerade(self):
         """
         Interpolationstest für eine Gerade durch die Punkte (0,0) und (0,1)
@@ -13,7 +12,13 @@ class TestPPscan(unittest.TestCase):
         """
         points = [(0, 0), (0, 1)]
         steps = 5
-        assert ppscan.interpol2d(points, steps) == [(0, 0), (0, .25), (0, .5), (0, .75), (0, 1)]
+        assert ppscan.interpol2d(points, steps) == [
+            (0, 0),
+            (0, 0.25),
+            (0, 0.5),
+            (0, 0.75),
+            (0, 1),
+        ]
 
     def test_interpol2d_parabel(self):
         """
@@ -23,7 +28,13 @@ class TestPPscan(unittest.TestCase):
         """
         points = [(-2, 4), (0, 0), (2, 4)]
         steps = 5
-        assert ppscan.interpol2d(points, steps) == [(-2.0, 4.0), (-1.0, 2.0), (0.0, 0.0), (1.0, 2.0), (2.0, 4.0)]
+        assert ppscan.interpol2d(points, steps) == [
+            (-2.0, 4.0),
+            (-1.0, 2.0),
+            (0.0, 0.0),
+            (1.0, 2.0),
+            (2.0, 4.0),
+        ]
 
     def test_neighbourhood_curvature_edge(self):
         """
@@ -45,10 +56,19 @@ class TestPPscan(unittest.TestCase):
         """
         img = ppscan.cv.imread("devel/l_01.jpg", 0)
         dim_x, dim_y = img.shape
-        val = ppscan.neighbourhood_curvature((dim_x - (dim_x / 2), dim_y - (dim_y / 2)), img, 10, 10)
+        val = ppscan.neighbourhood_curvature(
+            (dim_x - (dim_x / 2), dim_y - (dim_y / 2)), img, 10, 10
+        )
         assert 0.0 < val < 1.0
 
-    @parameterized.expand([("devel/l_01.jpg",), ("devel/l_04.jpg",), ("devel/r_03.jpg",), ("devel/r_08.jpg",)])
+    @parameterized.expand(
+        [
+            ("devel/l_01.jpg",),
+            ("devel/l_04.jpg",),
+            ("devel/r_03.jpg",),
+            ("devel/r_08.jpg",),
+        ]
+    )
     def test_find_valleys_count(self, file):
         """
         Überprüfung der Listenlänge von valleys (3) und deren Punkte (>=2)
@@ -58,9 +78,13 @@ class TestPPscan(unittest.TestCase):
         # aus find_keypoints() benötigter Code (l.52-59)
         img = ppscan.cv.imread(file, 0)
         blurred = ppscan.cv.GaussianBlur(img, (13, 13), 0)
-        _, thresh = ppscan.cv.threshold(blurred, (ppscan.THRESH_FACTOR * img.mean()), 255, ppscan.cv.THRESH_BINARY)
+        _, thresh = ppscan.cv.threshold(
+            blurred, (ppscan.THRESH_FACTOR * img.mean()), 255, ppscan.cv.THRESH_BINARY
+        )
         contours, _ = ppscan.cv.findContours(
-            thresh[:, : int(img.shape[1] / 2)], ppscan.cv.RETR_TREE, ppscan.cv.CHAIN_APPROX_SIMPLE
+            thresh[:, : int(img.shape[1] / 2)],
+            ppscan.cv.RETR_TREE,
+            ppscan.cv.CHAIN_APPROX_SIMPLE,
         )
         contours = [tuple(c[0]) for c in contours[0]]
         valleys = ppscan.find_valleys(thresh, contours)
