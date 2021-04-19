4ab405ce54492332560fd92beef031dda4a66ad3
JoProt <47474907+JoProt@users.noreply.github.com>
2021-04-19 08:58:48 +0200

    fixed black formatting

--- showROI.py
+++ showROI.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import ppscan
 import matplotlib.pyplot as plt
+
 img = cv.imread("devel/r_03.jpg", 0)
 k1, k2 = ppscan.find_keypoints(img)
 roi = ppscan.transform_to_roi(img, k2, k1)
