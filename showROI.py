f2361f4a0ef1ef8a2de6630b069c8408f00dfa8f
Josef <josef@neukaliss.de>
2021-04-21 18:41:59 +0200

    removed unnecessary sho_roi.py

--- showROI.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-import ppscan
-import matplotlib.pyplot as plt
-
-img = cv.imread("devel/r_03.jpg", 0)
-k1, k2 = ppscan.find_keypoints(img)
-roi = ppscan.transform_to_roi(img, k2, k1)
-ppscan.dbg_show(roi)
