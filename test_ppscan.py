85a68d314a04cfcb52cfb0775636087638cf3c00
Christoph Werner <christoph@werners.name>
2021-05-19 10:47:42 +0200

    cleaned up test_ppscan.py

--- test_ppscan.py
+++ test_ppscan.py
@@ -94,66 +94,48 @@ class TestPPscan(unittest.TestCase):
             # dass nur ein Punkt gefunden werden muss und der dann automatisch als valley gilt?
             assert len(valley) >= 2
 
-    def test_find_keypoints_l_dummy(self):
-        """
-        Test auf Bestimmung der Keypoints (zwischen Zeige- und Mittelfinger und zwischen Ringfinger und kleinem Finger)
-
-        :return: True bei richtiger Berechnung für Bild l_01.jpg
-        """
-        img = ppscan.cv.imread("devel/l_dummy.jpg", ppscan.cv.IMREAD_COLOR)
-        img = ppscan.cv.cvtColor(img, ppscan.cv.COLOR_BGR2GRAY)
-        k1, k2 = ppscan.find_keypoints(img, 1)
-        assert k1 == (169, 188)
-        assert k2 == (169, 81)
-
-    def test_find_keypoints_r_dummy(self):
-        """
-        Test auf Bestimmung der Keypoints (zwischen Zeige- und Mittelfinger und zwischen Ringfinger und kleinem Finger)
-
-        :return: True bei richtiger Berechnung für Bild l_01.jpg
-        """
-        img = ppscan.cv.imread("devel/r_dummy.jpg", ppscan.cv.IMREAD_COLOR)
-        img = ppscan.cv.cvtColor(img, ppscan.cv.COLOR_BGR2GRAY)
-        k1, k2 = ppscan.find_keypoints(img, 0)
-        assert k1 == (169, 209)
-        assert k2 == (169, 319)
+    # def test_find_keypoints_l_dummy(self):
+    #     """
+    #     Test auf Bestimmung der Keypoints (zwischen Zeige- und Mittelfinger und zwischen Ringfinger und kleinem Finger)
+    #
+    #     :return: True bei richtiger Berechnung für Bild l_01.jpg
+    #     """
+    #     img = ppscan.cv.imread("devel/l_dummy.jpg", ppscan.cv.IMREAD_COLOR)
+    #     img = ppscan.cv.cvtColor(img, ppscan.cv.COLOR_BGR2GRAY)
+    #     k1, k2 = ppscan.find_keypoints(img)
+    #     assert k1 == (169, 188)
+    #     assert k2 == (169, 81)
+    #
+    # def test_find_keypoints_r_dummy(self):
+    #     """
+    #     Test auf Bestimmung der Keypoints (zwischen Zeige- und Mittelfinger und zwischen Ringfinger und kleinem Finger)
+    #
+    #     :return: True bei richtiger Berechnung für Bild l_01.jpg
+    #     """
+    #     img = ppscan.cv.imread("devel/r_dummy.jpg", ppscan.cv.IMREAD_COLOR)
+    #     img = ppscan.cv.cvtColor(img, ppscan.cv.COLOR_BGR2GRAY)
+    #     k1, k2 = ppscan.find_keypoints(img)
+    #     assert k1 == (169, 209)
+    #     assert k2 == (169, 319)
 
     @parameterized.expand(
         [
             ("devel/l_01.jpg",),
             ("devel/l_04.jpg",),
+            ("devel/r_03.jpg",),
+            ("devel/r_08.jpg",),
         ]
     )
-    def test_transform_to_roi_left(self, file):
+    def test_extract_roi(self, file):
         """
         Testet ob ROI für linke Hand plausibel ist.
 
         :param file: vollständiges Handbild
         :return: True, wenn ROI dargestellt werden kann
         """
-        img = ppscan.cv.imread(file, ppscan.cv.IMREAD_GRAYSCALE)
-        k1, k2 = ppscan.find_keypoints(img, 1)
-        roi = ppscan.transform_to_roi(img, k2, k1)
-        assert roi.shape[0] > 0
-        assert roi.shape[1] > 0
-        assert roi.shape[0] == roi.shape[1]
+        img_input = ppscan.load_img(file)
+        roi = ppscan.extract_roi(img_input)
 
-    @parameterized.expand(
-        [
-            ("devel/r_03.jpg",),
-            ("devel/r_08.jpg",),
-        ]
-    )
-    def test_transform_to_roi_right(self, file):
-        """
-        Testet ob ROI für rechte Hand plausibel ist.
-
-        :param file: vollständiges Handbild
-        :return: True, wenn ROI dargestellt werden kann
-        """
-        img = ppscan.cv.imread(file, ppscan.cv.IMREAD_GRAYSCALE)
-        k1, k2 = ppscan.find_keypoints(img, 0)
-        roi = ppscan.transform_to_roi(img, k2, k1)
         assert roi.shape[0] > 0
         assert roi.shape[1] > 0
         assert roi.shape[0] == roi.shape[1]
@@ -169,20 +151,6 @@ class TestPPscan(unittest.TestCase):
         img = ppscan.cv.imread("devel/mask_dummy.jpg", ppscan.cv.IMREAD_GRAYSCALE)
         mask = ppscan.build_mask(img)
         assert mask.all() == img.all()
-
-    def test_apply_mask(self):
-        """
-        Wendet auf das Bild 'mask_dummy' (vertikal geteilt, eine Hälfte schwarz, andere weiß) das gleiche
-        Bild als Maske an. Ergebnis muss ein weißes Bild sein.
-
-        :return: True, wenn maskiertes Bild komplett weiß ist
-        """
-        img = ppscan.cv.imread("devel/mask_dummy.jpg", ppscan.cv.IMREAD_GRAYSCALE)
-        mask = img
-        masked = ppscan.apply_mask(img, mask)
-        white = ppscan.np.empty_like(img)
-        white.fill(255)
-        assert masked.all() == white.all()
 
     def test_papers_GABOR_CONST(self):
         """
